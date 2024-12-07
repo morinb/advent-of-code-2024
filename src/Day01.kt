6fb2181a34f206e492d7bb3f75fb71882accd7d6
Baptiste MORIN <1932256+morinb@users.noreply.github.com>
2024-12-07 13:26:34 +0100

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,34 @@
+import kotlin.math.abs
+
+/**
+ * Parses a list of space-separated string pairs into a pair of lists of longs.
+ *
+ * @param input A list of strings, where each string contains two space-separated numeric values.
+ * @return A pair of lists, where the first list contains the first numbers from the string pairs and the second list contains the second numbers.
+ * @throws IllegalArgumentException If a string in the input list does not contain exactly two space-separated values.
+ */
+fun listPairs(input: List<String>): Pair<List<Long>, List<Long>> =
+    input.map { line ->
+        line.split("""\s+""".toRegex()).let {
+            require(it.size == 2)
+            it[0].toLong() to it[1].toLong()
+        }
+    }.unzip()
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        return listPairs(input).let { (first, second) ->
+            first.sorted().zip(second.sorted()).map { (first, second) -> abs(first - second) }
+        }.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        return listPairs(input).let { (first, second) ->
+            first.sumOf { num -> num * second.count { it == num } }
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
